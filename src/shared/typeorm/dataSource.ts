ab8b5fccce8b25f2b4cf483cfcf0d4cc99df0419
michelzalotti <michelzalotti@gmail.com>
2024-05-05 15:29:39 -0300

    Criacao da Role Migration

--- src/shared/typeorm/dataSource.ts
+++ src/shared/typeorm/dataSource.ts
@@ -1,8 +1,9 @@
 import { DataSource } from 'typeorm';
+import { CreateRoleTable1714933498362 } from './migrations/1714933498362-CreateRoleTable';
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: 'db/app.db',
   entities: [],
-  migrations: [],
+  migrations: [CreateRoleTable1714933498362],
 });
---
+++ src/shared/typeorm/migrations/1714933498362-CreateRoleTable.ts
@@ -0,0 +1,37 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateRoleTable1714933498362 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'roles',
+        columns: [
+          {
+            name: 'id',
+            type: 'string',
+            isPrimary: true,
+          },
+          {
+            name: 'role',
+            type: 'string',
+            isUnique: true,
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updatedAt',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('roles');
+  }
+}
