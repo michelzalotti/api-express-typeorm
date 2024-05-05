8c3eb65b9468a9d5a72ab2c2e8f1fa33ee3733a1
michelzalotti <michelzalotti@gmail.com>
2024-05-05 20:13:59 -0300

    Criação da entidade Role

---
+++ src/domain/roles/entity/Role.ts
@@ -0,0 +1,28 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import { v4 as uuid } from 'uuid';
+
+@Entity('roles')
+export class Role {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  constructor() {
+    this.id = uuid();
+  }
+}
--- src/shared/typeorm/dataSource.ts
+++ src/shared/typeorm/dataSource.ts
@@ -1,9 +1,10 @@
 import { DataSource } from 'typeorm';
+import { Role } from '@domain/roles/entity/Role';
 import { CreateRoleTable1714933498362 } from './migrations/1714933498362-CreateRoleTable';
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: 'db/app.db',
-  entities: [],
+  entities: [Role],
   migrations: [CreateRoleTable1714933498362],
 });
