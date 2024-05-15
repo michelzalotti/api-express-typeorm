89bac4458801bb597215d236a3e8c7903ad90e48
Michel Zalotti <michelzalotti@gmail.com>
2024-05-15 12:24:48 -0300

    Criação da migration Users

--- src/shared/typeorm/dataSource.ts
+++ src/shared/typeorm/dataSource.ts
@@ -1,10 +1,11 @@
 import { DataSource } from 'typeorm';
 import { Role } from '@domain/roles/entity/Role';
 import { CreateRoleTable1714933498362 } from './migrations/1714933498362-CreateRoleTable';
+import { CreateUserTable1715786346402 } from './migrations/1715786346402-CreateUserTable';
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: 'db/app.db',
   entities: [Role],
-  migrations: [CreateRoleTable1714933498362],
+  migrations: [CreateRoleTable1714933498362, CreateUserTable1715786346402],
 });
---
+++ src/shared/typeorm/migrations/1715786346402-CreateUserTable.ts
@@ -0,0 +1,55 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUserTable1715786346402 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'string',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'string',
+          },
+          {
+            name: 'email',
+            type: 'string',
+            isUnique: true,
+          },
+          {
+            name: 'avatar',
+            type: 'string',
+            isNullable: true,
+          },
+          {
+            name: 'password',
+            type: 'string',
+          },
+          {
+            name: 'isAdmin',
+            type: 'boolean',
+            default: false,
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updatedAt',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
